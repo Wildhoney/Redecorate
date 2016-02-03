c2fa9fce656939afe03d3eebf9810f8819f80c8d
Wildhoney <adam.timberlake@gmail.com>
2016-02-03 19:43:31 +0000

    Added test for multiple properties

--- tests/apply.test.js
+++ tests/apply.test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import 'babel-core/register';
 import {apply} from '../component/redecorate';
 
-test('modifying a simple property', t => {
+test('modifying a single property', t => {
 
     const state = {
         name: { first: 'David', last: 'Bowie' },
@@ -11,5 +11,21 @@ test('modifying a simple property', t => {
 
     const transformed = apply(state)('name.last', () => 'Butterfield');
     t.same(transformed, { name: { first: 'David', last: 'Butterfield' }, age: 69 });
+
+});
+
+test('modifying a multiple properties', t => {
+
+    const state = {
+        name: { first: 'David', last: 'Bowie' },
+        gender: 'male',
+        age: 69
+    };
+
+    const firstTransformation = apply(state)('name.last', () => 'Butterfield');
+    const secondTransformation = apply(firstTransformation)('name.first', () => 'Jeremy');
+    const transformed = apply(secondTransformation)('gender', () => 'androgynous');
+
+    t.same(transformed, { name: { first: 'Jeremy', last: 'Butterfield' }, gender: 'androgynous', age: 69 });
 
 });
